78c8db53f7f4a8a0c1890fe436429f08588500b0
varunamachi <varunamachi@gmail.com>
2020-05-31 18:48:24 +0530

    No longer create multiple build dirs on Windows

--- build-unix.sh
+++ build-unix.sh
@@ -5,8 +5,31 @@ echo "Qt Install Prefix: $LINUX_QT5_INSTALL_PREFIX"
 echo "Travis build dir: $TRAVIS_BUILD_DIR"
 echo "Travos OS name: $TRAVIS_OS_NAME"
 
-../qt5/qtbase/configure -opensource -confirm-license -prefix "$LINUX_QT5_INSTALL_PREFIX" -static -release -ltcg -optimize-size -no-pch -sql-sqlite -qt-zlib -qt-libjpeg -qt-libpng -qt-pcre -qt-harfbuzz -skip webengine -nomake tools -nomake tests -nomake examples
+mkdir build
+cd build || exit 100
+
+../qt5/qtbase/configure \
+  -opensource \
+  -confirm-license \
+  -prefix "$LINUX_QT5_INSTALL_PREFIX" \
+  -static \
+  -release \
+  -ltcg \
+  -optimize-size \
+  -no-pch \
+  -sql-sqlite \
+  -qt-zlib \
+  -qt-libjpeg \
+  -qt-libpng \
+  -qt-pcre \
+  -qt-harfbuzz \
+  -skip webengine \
+  -nomake tools \
+  -nomake tests \
+  -nomake examples
+  
   # - ../qt5/qtbase/configure -opensource -confirm-license -prefix $QT5_INSTALL_PREFIX -release -static -silent -sql-sqlite -qt-zlib -qt-libjpeg -qt-libpng -qt-pcre -qt-harfbuzz -no-compile-examples -nomake tests -nomake examples -no-opengl -no-openssl -no-sql-odbc -no-icu -no-feature-concurrent -no-feature-xml -feature-testlib
+
 make -j2
 sudo make install
 "$QT5_INSTALL_PREFIX/bin/qmake" -v
--- build-windows.sh
+++ build-windows.sh
@@ -8,11 +8,27 @@ echo "Travos OS name: $TRAVIS_OS_NAME"
 mkdir build
 cd build || exit 100
 
-pwd
-ls -l ..
-  
 # - ..\qt5\qtbase\configure -opensource -confirm-license -platform win32-g++ -prefix %QT5_INSTALL_PREFIX% -release -static -static-runtime -silent -sql-sqlite -qt-zlib -qt-libjpeg -qt-libpng -qt-pcre -qt-harfbuzz
-../qt5/qtbase/configure -release -static -static-runtime -no-pch -opengl desktop -platform win32-g++ -prefix "$WINDOWS_QT5_INSTALL_PREFIX" -silent -sql-sqlite -qt-zlib -qt-libjpeg -qt-libpng -qt-pcre -qt-harfbuzz -skip webengine -nomake tools -nomake tests -nomake examples || exit 101
+../qt5/qtbase/configure \
+  -release \
+  -static \
+  -static-runtime \
+  -no-pch \
+  -opengl desktop \
+  -platform win32-g++ \
+  -prefix "$WINDOWS_QT5_INSTALL_PREFIX" \
+  -silent \
+  -sql-sqlite \
+  -qt-zlib \
+  -qt-libjpeg \
+  -qt-libpng \
+  -qt-pcre \
+  -qt-harfbuzz \
+  -skip webengine \
+  -nomake tools \
+  -nomake tests \
+  -nomake examples || exit 101
+  
 mingw32-make -j2 || exit 102
 mingw32-make install || exit 103
   
