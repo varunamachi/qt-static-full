99ef47353fd7af9ed49ef08212018fbe44412b7c
varunamachi <varunamachi@gmail.com>
2020-05-31 16:29:25 +0530

    Exporting and printing env vars

--- build-windows.sh
+++ build-windows.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-
+echo "Qt Version: $QT5_VERSION"
+echo "Qt Install Prefix: $QT5_INSTALL_PREFIX"
+echo "Travis build dir: $TRAVIS_BUILD_DIR"
+echo "Travos OS name: $TRAVIS_OS_NAME"
 
   mkdir build
   cd build || exit 100
@@ -9,4 +12,4 @@
   mingw32-make -j2 || 
   mingw32-make install
   dir
-  "$QT5_INSTALL_PREFIX\bin\qmake -v"
+  "$QT5_INSTALL_PREFIX/bin/qmake -v"
